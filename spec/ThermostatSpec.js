63dd3b062610260a04c97d97b122b8930b09adc1
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-05-03 11:24:56 +0100

    add feature of minimum temp of 10 degrees

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,27 +1,34 @@
+'use strict';
+
 describe('Thermostat', function(){
   var thermostat;
 
-  beforeEach( function(){
-    thermostat = new Thermostat()
-  };
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
 
   describe('initial temperature', function(){
     it('has temeprature of 20 degrees', function(){
-      expect(thermostat.temperature)toEqual(20)
+    expect(thermostat.temperature).toEqual(20)
     });
   });
 
-  describe('up',function(degrees){
-    it('increases temperature', function(degrees){
+  describe('up',function(){
+    it('increases temperature by 1 dg', function(){
       thermostat.up();
-      expect(thermostat.temperature)toEqual(25)
+      expect(thermostat.temperature).toEqual(21);
     });
   });
 
-  describe('down', function(degrees){
-    it('decreases temperature', function(degrees){
+  describe('down', function(){
+    it('decreases temperature by 1 dg', function(){
       thermostat.down();
-      expect(thermostat.temperature)toEqual(15)
+      expect(thermostat.temperature).toEqual(19)
+    });
+
+    it('has a minimum temp of 10 degrees', function(){
+      thermostat.temperature = 10
+      expect(function() {thermostat.down()}).toThrow('Temperature is too low')
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,15 +1,22 @@
+'use strict';
+
 function Thermostat() {
-  this.temperature = 20
+  this.temperature = 20;
+  this.minTemp = 10;
 }
 
-Thermostat.prototype.getstartTemp = function() {
+Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
 };
 
-Thermostat.prototype.up = function(degrees) {
-  this.temperature += degrees
+Thermostat.prototype.up = function() {
+  return this.temperature += 1;
 };
 
-Thermostat.prototype.down = function(degrees) {
-  this.temperature -= degrees
+Thermostat.prototype.down = function() {
+  if (this.temperature === 10) {
+    throw 'Temperature is too low';
+  } else {
+    return this.temperature -= 1;
+ }
 };
